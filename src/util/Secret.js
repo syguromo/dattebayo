06896e99585e0e36078a8c9e73017afa9e30f4d8
syguromo <hz.nguen@gmail.com>
2022-09-09 18:44:25 +0300

    Added secret phrase

--- src/util/Secret.js
+++ src/util/Secret.js
@@ -1,17 +1,19 @@
 import { AES, enc } from 'crypto-js';
 
 export const decrypt = (message, key) => {
-    const dec = AES.decrypt(message, key)
-    let result = dec.toString(enc.Utf8)
+    try {
+        const dec = AES.decrypt(message, key)
+        let result = dec.toString(enc.Utf8)
 
-    if (result.length === 0) {
-        return 0
-    } else
-        return result
+        if (result.length === 0) {
+            return false
+        } else
+            return result
+    } catch (error) {
+        return false
+    }
 }
 
 export const encrypt = (message, key) => {
-    
-
     return AES.encrypt(message, key).toString()
 }
